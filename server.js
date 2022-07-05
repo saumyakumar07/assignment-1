fffaba09f50071ddcc2e5c84cee563f8f044d925
saumya kumar <97030706+saumyakumar07@users.noreply.github.com>
2022-07-05 22:47:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Block = require('./block');
 const blockchain = new Blockchain();
 
 for(let i=0; i<5; i++) {
-    const newData = 'krunal'+i;
+    const newData = 'saumya'+i;
     blockchain.addBlock({data: newData});
 }
 
